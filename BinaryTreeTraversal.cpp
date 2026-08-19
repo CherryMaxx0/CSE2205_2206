10bad6d6e3ddfb76ce986ea658d8cda5e4b5f94e
cherrymaxx0 <ariksab007@gmail.com>
2026-08-19 22:53:48 +0600

    bst assignment

---
+++ BinaryTreeTraversal.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BST
+{
+public:
+    Node *root;
+
+    BST()
+    {
+        root = NULL;
+    }
+
+    Node *insert(Node *root, int value)
+    {
+        if (root == NULL)
+        {
+            return new Node(value);
+        }
+
+        if (value < root->data)
+        {
+            root->left = insert(root->left, value);
+        }
+        else
+        {
+            root->right = insert(root->right, value);
+        }
+
+        return root;
+    }
+
+    void inorder(Node *root)
+    {
+        if (root != NULL)
+        {
+            inorder(root->left);
+            cout << root->data << " ";
+            inorder(root->right);
+        }
+    }
+
+    void preorder(Node *root)
+    {
+        if (root != NULL)
+        {
+            cout << root->data << " ";
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+
+    void postorder(Node *root)
+    {
+        if (root != NULL)
+        {
+            postorder(root->left);
+            postorder(root->right);
+            cout << root->data << " ";
+        }
+    }
+};
+
+int main()
+{
+    BST tree;
+
+    int n, value;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value;
+        tree.root = tree.insert(tree.root, value);
+    }
+
+    cout << "Inorder traversal: ";
+    tree.inorder(tree.root);
+    cout << endl;
+
+    cout << "Preorder traversal: ";
+    tree.preorder(tree.root);
+    cout << endl;
+
+    cout << "Postorder traversal: ";
+    tree.postorder(tree.root);
+    cout << endl;
+
+    return 0;
+} 
