84a9bf0a01773bf9346cdb7a93c5210d90afc5c4
cherrymaxx0 <ariksab007@gmail.com>
2026-08-19 22:59:45 +0600

    adjusted

--- BinaryTreeTraversal.cpp
+++ BinaryTreeTraversal.cpp
@@ -8,67 +8,44 @@ public:
     Node *left;
     Node *right;
 
-    Node(int value)
-    {
+    Node(int value) {
         data = value;
         left = NULL;
         right = NULL;
     }
 };
 
-class BST
-{
+class BST {
 public:
     Node *root;
 
-    BST()
-    {
+    BST() {
         root = NULL;
     }
 
-    Node *insert(Node *root, int value)
-    {
-        if (root == NULL)
-        {
-            return new Node(value);
-        }
-
-        if (value < root->data)
-        {
-            root->left = insert(root->left, value);
-        }
-        else
-        {
-            root->right = insert(root->right, value);
-        }
-
+    Node *insert(Node *root, int value) {
+        if (root == NULL) return new Node(value);
+        if (value < root->data) root->left = insert(root->left, value);
+        else root->right = insert(root->right, value);
         return root;
     }
-
-    void inorder(Node *root)
-    {
-        if (root != NULL)
-        {
+    void inorder(Node *root) {
+        if (root != NULL) {
             inorder(root->left);
             cout << root->data << " ";
             inorder(root->right);
         }
     }
-
-    void preorder(Node *root)
-    {
-        if (root != NULL)
-        {
+    void preorder(Node *root) {
+        if (root != NULL) {
             cout << root->data << " ";
             preorder(root->left);
             preorder(root->right);
         }
     }
 
-    void postorder(Node *root)
-    {
-        if (root != NULL)
-        {
+    void postorder(Node *root) {
+        if (root != NULL) {
             postorder(root->left);
             postorder(root->right);
             cout << root->data << " ";
@@ -76,19 +53,15 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     BST tree;
-
     int n, value;
 
     cout << "Enter number of elements: ";
     cin >> n;
-
     cout << "Enter " << n << " elements: ";
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> value;
         tree.root = tree.insert(tree.root, value);
     }
